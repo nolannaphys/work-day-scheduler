b907a0ba7114948aef8e1409dfc8b8c8ea4ec57f
Nolan <nolannaphys@gmail.com>
2023-04-20 14:28:42 -0400

    js updated with ids for each hour, dayjs, and local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,39 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
+  let timeSlot = [
+    {id: "#hour-9", hour: 9},
+    {id: "#hour-10", hour: 10},
+    {id: "#hour-11", hour: 11},
+    {id: "#hour-12", hour: 12},
+    {id: "#hour-13", hour: 13},
+    {id: "#hour-14", hour: 14},
+    {id: "#hour-15", hour: 15},
+    {id: "#hour-16", hour: 16},
+    {id: "#hour-17", hour: 17},
+  ];
+
+  let retrieveUserEntry = [];
+
+  let currentYear = dayjs().$y;
+  let currentMonth = dayjs().format('MMMM');
+  let currentDayOfMonth = dayjs().$D;
+  let currentDay = dayjs().format('dddd');
+  let currentHour = dayjs().format('HH');
+  let todaysDate = dayjs().format('dddd, MMMM D, YYYY');
+
+  if (localStorage.getItem('day-planner-events') !== null){
+    retrieveUserEntry = JSON.parse(localStorage.getItem('day-planner-events'));
+    localStorage.setItem('day-planner-events', JSON.stringify(retrieveUserEntry));
+
+    $.each(retrieveUserEntry, function (key, value){
+      if (value.day === todaysDate){
+        let entryHourId = `#${value.hour}`;
+        $(entryHourId).find('textarea').text(value.event);
+      }
+    });
+  }
+
     // TODO: Add a listener for click events on the save button. This code should
     // use the id in the containing time-block as a key to save the user input in
     // local storage. HINT: What does `this` reference in the click listener
@@ -26,6 +59,3 @@ $(function () {
       console.log('test')
     }
     setInterval(dateTime, 1000);
-  
-
- 
